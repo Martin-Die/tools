bed3554edaf048471c65097f0b9207ab210bc844
Martin-Die <martin.die.7559@gmail.com>
2024-11-21 10:14:53 +0100

    config changé 2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
-  base: "/tools/"
+  // base: "/tools/"
 })
